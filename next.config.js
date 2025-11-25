6ebdd3459f0588804d63aea749d3107d86dae535
Kimchhay108 <thoeurn.kimchhay.2823@rupp.edu.kh>
2025-11-25 12:51:21 +0700

    config every file and rename

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Add any other config options here
 };
 
-export default nextConfig;
+module.exports = nextConfig;
